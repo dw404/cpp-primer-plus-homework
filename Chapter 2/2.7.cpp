f01627f365a9c9e83762b0cac3d22e8558057f5a
dw404 <747906511@qq.com>
2023-05-16 21:06:54 +0800

    finish 2.7

---
+++ Chapter 2/2.7.cpp
@@ -0,0 +1,11 @@
+﻿#include<iostream>
+
+int main() {
+	int h=0, m=0;
+	std::cout << "Enter the number of hours: ";
+	std::cin >> h;
+	std::cout << "Enter the number of minuters: ";
+	std::cin >> m;
+	std::cout << "Time: " << h << ":" << m;
+	return 0;
+}
