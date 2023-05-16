246d0c09fbbd5d900e54e44a510526add05d17ae
dw404 <747906511@qq.com>
2023-05-16 15:50:45 +0800

    finish 2.6

---
+++ Chapter 2/2.5.cpp
@@ -0,0 +1,13 @@
+﻿#include<iostream>
+double transfer(double );
+int main()
+{
+	std::cout << "Please enter a Celsius value: ";
+	double x;
+	std::cin >> x;
+	std::cout << x << " degrees Celsius is " << transfer(x) << " degrees Fahrenheit.";
+	return 0;
+}
+double transfer(double x) {
+	return x * 1.8 + 32.0;
+}
---
+++ Chapter 2/2.6.cpp
@@ -0,0 +1,11 @@
+﻿#include<iostream>
+double convert(double x) {
+	return x * 63240;
+}
+int main() {
+	std::cout << "Enter the number of light years: ";
+	double x;
+	std::cin >> x;
+	std::cout << x << " light years = " << convert(x) << " astronomical units.";
+	return 0;
+}
