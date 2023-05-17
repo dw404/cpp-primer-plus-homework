6da4ca130f0afa921260b4f69fe0189c07853f28
dw404 <747906511@qq.com>
2023-05-17 15:09:00 +0800

    finish 3.1

---
+++ Chapter 3/3.1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+const int u = 12;
+int main() {
+    cout << "Enter:_";
+    int h = 0;
+    cin >> h;
+    cout << "Height is " << h / u << " foot and " << h % u << " inch";
+
+    return 0;
+}
